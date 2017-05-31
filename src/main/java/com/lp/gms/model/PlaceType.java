d4afba690262ff6d8ca078d09331d73a135da65f
shilim <1251314045@qq.com>
2017-05-31 21:12:37 +0800

    2017.5.31 增加场地类型接口，修改页面目录机构，分admin和front

---
+++ src/main/java/com/lp/gms/model/PlaceType.java
@@ -0,0 +1,24 @@
+package com.lp.gms.model;
+
+public class PlaceType {
+    private Integer id;
+
+    private String placeTypeName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPlaceTypeName() {
+		return placeTypeName;
+	}
+
+	public void setPlaceTypeName(String placeTypeName) {
+		this.placeTypeName = placeTypeName;
+	}
+
+}
---
+++ src/main/java/com/lp/gms/service/PlaceTypeService.java
@@ -0,0 +1,49 @@
+package com.lp.gms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lp.gms.constant.ResultCode;
+import com.lp.gms.dao.PlaceTypeDao;
+import com.lp.gms.model.Page;
+import com.lp.gms.model.PageInfo;
+import com.lp.gms.model.PlaceType;
+import com.lp.gms.model.ResultMessage;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class PlaceTypeService {
+	@Autowired
+	private PlaceTypeDao placeTypeDao;
+	
+	public ResultMessage addPlaceType(PlaceType placeType) throws Exception {
+		placeTypeDao.insert(placeType);
+		return new ResultMessage(true, ResultCode.SUCCESS, "添加成功", null);
+	}
+	
+	public ResultMessage deletePlaceType(List<PlaceType> placeTypes) throws Exception {
+		placeTypeDao.deleteByList(placeTypes);
+		return new ResultMessage(true, ResultCode.SUCCESS, "删除成功", null);
+	}
+	
+	public ResultMessage updatePlaceType(PlaceType placeType) throws Exception {
+		placeTypeDao.insert(placeType);
+		return new ResultMessage(true, ResultCode.SUCCESS, "修改成功", null);
+	}
+	
+	public ResultMessage selectPlaceTypeList(Page page) throws Exception {
+		page.coutStartColum();
+		long total = placeTypeDao.selectCount(page);
+		List<PlaceType> placeTypes = placeTypeDao.selectByPage(page);
+		PageInfo<PlaceType> pageInfo = new PageInfo<PlaceType>(page, total, placeTypes);
+		return new ResultMessage(true, ResultCode.SUCCESS, "分页成功", pageInfo);
+	}
+	
+	public ResultMessage selectOnePlaceType(PlaceType placeType) throws Exception {
+		PlaceType rPlaceType = placeTypeDao.selectByPrimaryKey(placeType.getId());
+		return new ResultMessage(true, ResultCode.SUCCESS, "查询场地类型成功", rPlaceType);
+	}
+}
