6de35ad18d39f7f795b5cb95750ab0ed4e018081
shilim <1251314045@qq.com>
2017-06-10 17:14:19 +0800

    2017.6.10 合并2.0，帮jerry修理bug，上传了数据文件gms.sql

--- src/main/webapp/js/controller/front/indexApp.js
+++ src/main/webapp/js/controller/front/indexApp.js
@@ -13,32 +13,26 @@ indexApp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$
 		templateUrl:"place/place-lease.html",
 		controller:"leasePlace"
 	})
-<<<<<<< HEAD
 	.state("placeLeaseRecordList",{
 		url:"/placeLeaseRecordList",
 		templateUrl:"place-lease-record/place-lease-record-list.html",
 		controller:"placeLeaseRecordList"
-	});
-=======
-	
+	})
 	.state("equipmentList",{
 		url:"/equipmentList",
 		templateUrl:"equipment/equipment-list.html",
 		controller:"equipmentList"
 	})
-	
 	.state("leaseEquipment",{
 		url:"/leaseEquipment/:equipmentid",
 		templateUrl:"equipment/equipment-lease.html",
 		controller:"leaseEquipment"
 	})
-	
 	.state("equipmentloanList",{
 		url:"/equipmentloanList",
 		templateUrl:"equipmentloan/equipmentloan-list.html",
 		controller:"equipmentloanList"
-	})
->>>>>>> origin/jerry
+	});
 }])
 .config(['$httpProvider',function($httpProvider) {
 	$httpProvider.defaults.transformRequest=function(obj){
